7753bb7cae493c46afebf170dca31cb9bada3000
rekky1aws <killikomfr@gmail.com>
2025-10-02 22:29:28 +0200

    Task entity

---
+++ src/Entity/Task.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTime $date = null;
+
+    #[ORM\Column]
+    private ?int $signifiance = null;
+
+    #[ORM\Column]
+    private ?int $urgency = null;
+
+    #[ORM\Column]
+    private ?bool $isDone = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTime $doneDate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTime
+    {
+        return $this->date;
+    }
+
+    public function setDate(?\DateTime $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getSignifiance(): ?int
+    {
+        return $this->signifiance;
+    }
+
+    public function setSignifiance(int $signifiance): static
+    {
+        $this->signifiance = $signifiance;
+
+        return $this;
+    }
+
+    public function getUrgency(): ?int
+    {
+        return $this->urgency;
+    }
+
+    public function setUrgency(int $urgency): static
+    {
+        $this->urgency = $urgency;
+
+        return $this;
+    }
+
+    public function isDone(): ?bool
+    {
+        return $this->isDone;
+    }
+
+    public function setIsDone(bool $isDone): static
+    {
+        $this->isDone = $isDone;
+
+        return $this;
+    }
+
+    public function getDoneDate(): ?\DateTime
+    {
+        return $this->doneDate;
+    }
+
+    public function setDoneDate(?\DateTime $doneDate): static
+    {
+        $this->doneDate = $doneDate;
+
+        return $this;
+    }
+}
