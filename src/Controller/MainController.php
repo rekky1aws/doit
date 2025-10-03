7491f1aa33a8d28fae5dcc51bc49ea60de6d5044
rekky1aws <killikomfr@gmail.com>
2025-10-03 16:39:36 +0200

    Basic tasks view with fallback
    View and controller to display tasks.
    Fallback in view if there are no tasks in db.

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class MainController extends AbstractController
 {
-    #[Route('/', name: 'app_main')]
-    public function index(): Response
-    {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
-    }
+  #[Route('/', name: 'app_main')]
+  public function index(EntityManagerInterface $entityManager): Response
+  {
+    $tasks = $entityManager->getRepository(Task::class)->findAll();
+
+    return $this->render('main/index.html.twig', [
+      'page_title' => 'Home',
+      'tasks' => $tasks,
+    ]);
+  }
 }
