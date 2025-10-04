f24c0651dfbcf97b88cdf472277cbaaea0e6230d
rekky1aws <killikomfr@gmail.com>
2025-10-04 17:34:06 +0200

    Task form
     + Form to create and edit tasks.
     + View to create and edit tasks (WIP).
     + Edit controller.

---
+++ src/Controller/EditController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Task;
+use App\Form\EditTaskType;
+use Doctrine\ORM\EntityManagerInterface;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class EditController extends AbstractController
+{
+    #[Route('/edit', name: 'app_edit')]
+    public function edit(): Response
+    {
+        $form = $this->createForm(EditTaskType::class);
+
+        return $this->render('edit/index.html.twig', [
+            'task_form' => $form,
+            'controller_name' => 'basic edit',
+        ]);
+    }
+
+    #[Route('/edit/{id}', name: 'app_edit_by_id')]
+    public function editById (EntityManagerInterface $entityManager, int $id): Response
+    {
+        $task = $entityManager->getRepository(Task::class)->find($id);
+
+        $form = $this->createForm(EditTaskType::class, $task);
+
+        return $this->render('edit/index.html.twig', [
+            'task_form' => $form,
+            'controller_name' => 'edit task '.$id,
+        ]);
+    }
+}
---
+++ src/Form/EditTaskType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Group;
+use App\Entity\Tag;
+use App\Entity\Task;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EditTaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('date')
+            ->add('signifiance')
+            ->add('urgency')
+            ->add('isDone')
+            ->add('doneDate')
+            ->add('taskGroup', EntityType::class, [
+                'class' => Group::class,
+                'choice_label' => 'id',
+            ])
+            ->add('tags', EntityType::class, [
+                'class' => Tag::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
