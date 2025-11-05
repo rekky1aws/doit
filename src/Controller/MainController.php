dd28a108530d1119dfa0b5e6328cf4347cb6b0e7
rekky1aws <killikomfr@gmail.com>
2025-11-05 04:30:35 +0100

    Comments in MainController
    Added TODO comments in MainController about sorting Tasks

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -15,6 +15,9 @@ final class MainController extends AbstractController
   public function index(EntityManagerInterface $entityManager): Response
   {
     $tasks = $entityManager->getRepository(Task::class)->findAll();
+    
+    // TODO : Sort Tasks before sending : shortest delay => large delay => no date => done.
+    // TODO : Add a way to change how tasks are sorted with GET parameters.
 
     return $this->render('main/index.html.twig', [
       'page_title' => 'Home',
